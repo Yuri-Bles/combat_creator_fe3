d9bc236bf3ff1b7fcdfd19ff4c7bcc03bc68357b
Yuri-Bles <553227@student.fontys.nl>
2025-10-31 13:20:56 +0100

    Test Push

--- app/Components/Button.tsx
+++ app/Components/Button.tsx
@@ -9,7 +9,7 @@ export default function Button({ label, onClick, type = "button" }: ButtonProps)
     <button
       type={type}
       onClick={onClick}
-      className="px-3 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700"
+      className="px-2 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700"
     >
       {label}
     </button>
